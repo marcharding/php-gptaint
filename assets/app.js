8dd22fab81351d101c71d809a19d019c060f6e4f
Marc Harding <info@marcharding.de>
2023-07-08 18:05:30 +0200

    Add encore and tailwind.

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+import './styles/app.scss';
+
+// Alpine
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    plugins: [],
+}
