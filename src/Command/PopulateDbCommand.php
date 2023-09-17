e480a42846f5fde367bf37a9ac595394ae1ab643
Marc Harding <info@marcharding.de>
2023-09-17 14:54:46 +0200

    Formatting.

--- src/Command/PopulateDbCommand.php
+++ src/Command/PopulateDbCommand.php
@@ -8,8 +8,6 @@ use App\Service\CodeExtractor\CodeExtractor;
 use App\Service\SarifToFlatArrayConverter;
 use App\Service\TaintTypes;
 use Doctrine\ORM\EntityManagerInterface;
-use Gioni06\Gpt3Tokenizer\Gpt3Tokenizer;
-use Gioni06\Gpt3Tokenizer\Gpt3TokenizerConfig;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -64,6 +62,8 @@ class PopulateDbCommand extends Command
                 // Extract pluginname from readme.txt
                 $name = $this->extractPluginnameFromReadme($fileInfo);
 
+                $io->info("$name ($folderName)");
+
                 $s = new SarifToFlatArrayConverter();
                 $psalmResultFile = $this->projectDir . DIRECTORY_SEPARATOR . 'data/wordpress/sarif' . DIRECTORY_SEPARATOR . $folderName . '.sarif';
                 $psalmResultFile = json_decode(file_get_contents($psalmResultFile), true);
@@ -92,18 +92,19 @@ class PopulateDbCommand extends Command
                     $issueEntity->setFile($issue['file']);
                     $issueEntity->setDescription($issue['description']);
                     $issueEntity->setPsalmResult($issue['content']);
+                    $io->text($folderName . DIRECTORY_SEPARATOR . $issue['file']);
 
                     // store optimized codepath and unoptimized for token comparison / effectiveness
                     $extractedCodePath = "";
                     $unoptimizedCodePath = "";
-                    if(isset($sarifResults[$issue['errorId'].'_'.$issue['file']])){
-                        $entry = $sarifResults[$issue['errorId'].'_'.$issue['file']];
+                    if (isset($sarifResults[$issue['errorId'] . '_' . $issue['file']])) {
+                        $entry = $sarifResults[$issue['errorId'] . '_' . $issue['file']];
                         foreach ($entry["locations"] as $item) {
                             $pluginRoot = $this->projectDir . DIRECTORY_SEPARATOR . 'data/wordpress/plugins_tainted' . DIRECTORY_SEPARATOR . $folderName . DIRECTORY_SEPARATOR;
                             $extractedCodePath .= "// FILE: {$item['file']}" . PHP_EOL . PHP_EOL . PHP_EOL;
-                            $extractedCodePath .= $this->codeExtractor->extractCodeLeadingToLine($pluginRoot.$item['file'],  $item["region"]['startLine']);
+                            $extractedCodePath .= $this->codeExtractor->extractCodeLeadingToLine($pluginRoot . $item['file'], $item["region"]['startLine']);
                             $extractedCodePath .= PHP_EOL . PHP_EOL . PHP_EOL;
-                            $unoptimizedCodePath .= file_get_contents($pluginRoot.$item['file']);
+                            $unoptimizedCodePath .= file_get_contents($pluginRoot . $item['file']);
                         }
                     }
 
@@ -133,14 +134,14 @@ class PopulateDbCommand extends Command
     {
         $readmeFile = $fileInfo->getPathname() . '/readme.txt';
 
-        if(!is_file($fileInfo->getPathname() . '/readme.txt')){
+        if (!is_file($fileInfo->getPathname() . '/readme.txt')) {
             return $fileInfo->getBasename();
         }
 
         $readmeContent = file_get_contents($readmeFile);
         preg_match('/===\s*([^=]+)\s*===/', $readmeContent, $matches);
 
-        if(empty($matches)){
+        if (empty($matches)) {
             return $fileInfo->getBasename();
         }
 
