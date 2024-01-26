0d9f6c5e35787fc8d260caf13e45eaccfebe61cc
Marc Harding <info@marcharding.de>
2024-01-26 17:00:52 +0100

    Add new gpt models.

--- src/Service/GptQuery.php
+++ src/Service/GptQuery.php
@@ -106,7 +106,15 @@ class GptQuery
                 '4k' => 'gpt-3.5-turbo-0613',
                 '16k' => 'gpt-3.5-turbo-16k-0613',
             ],
+            'gpt-3.5-turbo-0125' => [
+                '4k' => 'gpt-3.5-turbo-0613',
+                '16k' => 'gpt-3.5-turbo-16k-0613',
+            ],
             'gpt-4-1106-preview' => [
+                '4k' => 'gpt-4-1106-preview',
+                '16k' => 'gpt-4-1106-preview',
+            ],
+            'gpt-4-0125-preview' => [
                 '4k' => 'gpt-4-1106-preview',
                 '16k' => 'gpt-4-1106-preview',
             ],
